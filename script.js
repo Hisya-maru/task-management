28e28bd17186214e2b8c2c07ecc5a50e934493df
Hisya-maru <Fujibayasi.nmtoy1120@gmail.com>
2026-05-11 14:36:52 +0900

    ダミーデータ作成

---
+++ script.js
@@ -0,0 +1,7 @@
+//ダミーデータ
+const tasks = [
+    {id:1,title:"課題提出",priority:"S",completed:false,urgency:5},
+    {id:2,title:"ES作成",priority:"B",completed:true,urgency:4},
+    {id:3,title:"面接対策",priority:"A",completed:false,urgency:1},
+    {id:4,title:"ポートフォリオ作成",priority:"C",completed:true,urgency:5}
+];
